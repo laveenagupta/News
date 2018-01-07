98699178f90077bb89dd8b0c08acf1c30c148934
laveenagupta <laveenagupta008@gmail.com>
2018-01-07 17:40:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script>
+$(document).ready(function(){
+   // $("#news").click(function(){
+        $.getJSON("https://newsapi.org/v2/top-headlines?sources=the-next-web,the-verge&apiKey=53f4eea732ad4cd7a90d4fe17f728e9e", function(result){
+
+		$(result).each(function(i,result){
+			$.each(result,function(k,v){
+			for (item in result) { 
+				$i="1";		for (subItem in result[item]) { 
+				if (typeof result[item][subItem].title !== "undefined") {
+				       $('#newsData tr:last').after("<tr><td>"+result[item][subItem].title+"</td><td>"+result[item][subItem].description+"</td><td>"+result[item][subItem].publishedAt+"</td><td>"+result[item][subItem].author+"</td><td><img src='"+result[item][subItem].urlToImage+"' alt='image' width='100px;' height='100px;' /></td><td><form action='detail.php' method='post'><input type='hidden' name='title' value="+result[item][subItem].title+"/><input type='submit' value='View Detail' class='btn btn-warning'/></form></td></tr>");
+				}
+					//$("div").append("<b>"+result[item][subItem].title+"</b>");
+					
+						console.log(result[item][subItem]);
+						console.log(result[item][subItem].description);
+						console.log(result[item][subItem].publishedAt);
+						console.log(result[item][subItem].author);
+						console.log(result[item][subItem].urlToImage);
+					
+				}
+				}
+				
+			});
+        });
+    });
+
+
+//});
+  });
+</script>
+</head>
+<body>
+<div class="container">
+  <h2 id="news">News</h2>
+          
+  <table class="table" id="newsData">
+    <thead>
+      <tr>
+        <th>News Headline</th>
+        <th>News Description</th>
+        <th>Date</th>
+        <th>Author</th>
+        <th>Image</th>
+		<th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+		<td></td>
+	  
+	  </tr>
+	  
+      
+    </tbody>
+  </table>
+  
+</div>
+
+</body>
+</html>
